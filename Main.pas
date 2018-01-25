239ab8115988be62b30d07af443171bf4d42b1a9
Никита <Don.Jugger@gmail.com>
2018-01-25 13:05:34 +0400

    Первая версия задания массива входных данных для сортировки методом пузырька

---
+++ Main.pas
@@ -0,0 +1,15 @@
+const
+    m = 10;
+ 
+var
+    arr: array[1..m] of integer;
+    i: integer;
+begin
+    randomize;
+ 
+    write ('Исходный массив: ');
+    for i := 1 to m do begin
+        arr[i] := random(256);
+        write (arr[i]:4);
+    end;
+end.
